9ecaa421a31d5825432aa1da4eef0f571920551e
macsrd <maciejsrodulski@outlook.com>
2021-08-23 10:01:50 +0200

    My first Django app, first commit

---
+++ users/urls.py
@@ -0,0 +1,12 @@
+"""Definiuje wzorce adresów URL dla aplikacji users."""
+
+from django.urls import path, include
+
+from . import views
+
+app_name = 'users'
+urlpatterns = [
+	path('', include('django.contrib.auth.urls')),
+	# Strona rejestracji.
+	path('register/', views.register, name='register'),
+]
